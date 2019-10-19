d11e65acd13bb1fd48ecee856f08ea1adb203d78
Adam Kiss <kiss.adam.a@gmail.com>
2019-10-19 13:20:00 +0200

    Add HTTPS

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(4000);
+  const fs = require('fs');
+  const httpsOptions = {
+    key: fs.readFileSync('src/secrets/aws/server.key'),
+    cert: fs.readFileSync('src/secrets/aws/server.crt'),
+  }
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+    cors: true,
+  });
+  await app.listen(443);
 }
 bootstrap();
