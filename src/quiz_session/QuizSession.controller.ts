6f09e345faede152679bc94dbb1dffcc83ece0df
Adam Kiss <kiss.adam.a@gmail.com>
2019-10-28 22:13:49 +0100

    adjust endpoints

--- src/quiz_session/QuizSession.controller.ts
+++ src/quiz_session/QuizSession.controller.ts
@@ -1,26 +1,26 @@
 import { Controller, Post, Param, Body, Get } from '@nestjs/common';
 import { QuizSessionService } from './QuizSession.service';
 
-@Controller('quizzes')
+@Controller('quiz-sessions')
 export class QuizSessionController {
     constructor(
         private readonly quizService: QuizSessionService
     ) { }
 
-    @Post('/:quizId/quizSessions/')
+    @Post('/by-quizzes/:quizId')
     async createSessionForQuiz(@Param('quizId') quizId) {
         return {
             sessionId: await this.quizService.createNewSessionForQuiz(quizId)
         }
     }
 
-    @Get('/quizSessions/:sessionId/nextQuestion')
+    @Get('/:sessionId/nextQuestion')
     getNextQuestionForQuiz(@Param('sessionId') sessionId) {
       return this.quizService.getNextQuestionForSession(sessionId);
     }
     
-    @Post('/:quizId/clientAnswers')
-    verifyAnswer(@Param('quizId') quizId, @Body() body: any) {
-        return this.quizService.validateAnswerForQuestion(quizId, body.questionId, body.answerId);
+    @Post('/:sessionId/clientAnswers')
+    verifyAnswer(@Param('sessionId') sessionCode, @Body() body: any) {
+        return this.quizService.validateAnswerForQuestion(sessionCode, body.questionId, body.answerId);
     }
 }
